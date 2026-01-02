dd4862cc0a0bcd5cb23ca2e454939ee16f9c3715
ales2026 <ale.urankar@gmail.com>
2026-01-02 16:13:55 +0100

    adding starfield

--- script.js
+++ script.js
@@ -43,6 +43,23 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.03;
 
+function assStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({color: 0xffffff});
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(assStar);
+
+
+
+
+
 
 
 ///////////////////////////////////////////////
