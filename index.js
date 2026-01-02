02d4c72e817836eddc31e7f0d8437d0cf3484239
ales2026 <ale.urankar@gmail.com>
2026-01-02 07:07:40 +0100

    cube with background

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import * as THREE from "three";
+import getLayer from "./getLayer.js";
 import { OrbitControls } from "jsm/controls/OrbitControls.js";
 
-const w = window.innerHeight;
+const w = window.innerWidth;
 const h = window.innerHeight;
-const renderer = new THREE.WebGLRenderer({ antialias: true });
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 const fov = 75;
 const aspect = w / h;
 const near = 0.1;
-const far = 10;
+const far = 100;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.z = 2;
 const scene = new THREE.Scene();
@@ -18,7 +19,7 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.03;
 
-const geo = new THREE.IcosahedronGeometry(1.0,2);
+const geo = new THREE.BoxGeometry();
 const mat = new THREE.MeshStandardMaterial({
   color: 0xffffff,
   flatShading: true
@@ -26,23 +27,33 @@ const mat = new THREE.MeshStandardMaterial({
 const mesh = new THREE.Mesh(geo, mat);
 scene.add(mesh);
 
-const wireMat = new THREE.MeshBasicMaterial({
-  color: 0xffffff,
-  wireframe: true
-})
-
-const wireMesh = new THREE.Mesh(geo, wireMat);
-wireMesh.scale.setScalar(1.0001)
-mesh.add(wireMesh);
-
 const hemiLight = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
 scene.add(hemiLight);
 
+// Sprites BG
+const gradientBackground = getLayer({
+  hue: 0.5,
+  numSprites: 8,
+  opacity: 0.2,
+  radius: 10,
+  size: 24,
+  z: -15.5,
+});
+scene.add(gradientBackground);
+
 function animate(t = 0) {
   requestAnimationFrame(animate);
+  mesh.rotation.x += 0.002;
   mesh.rotation.y = t * 0.0001;
   renderer.render(scene, camera);
   controls.update();
 }
 
 animate();
+
+function handleWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', handleWindowResize, false);
