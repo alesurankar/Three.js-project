21e61562451618733a20bdd8a8eb8224aeaa33c5
ales2026 <ale.urankar@gmail.com>
2026-03-01 08:47:48 +0100

    adding gravityCenter to database and changing keys for alphaCentauri

---
+++ universe2/server/seed/gravitycenters.js
@@ -0,0 +1,12 @@
+
+export const blackholes = [
+    {
+        key: "alpha_centauri_center",
+        name: "Alpha Centauri Barry Center*",
+        type: "gravity_center",
+        parentKey: "sagittarius_a",
+        systemKey: "alpha_centauri_system",
+        galaxyKey: "milky_way",
+        size: 600000,
+    },
+]
