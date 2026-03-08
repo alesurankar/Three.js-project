0d4486cc7416e936e59f8917dcd6c40f0b844b3b
ales2026 <ale.urankar@gmail.com>
2026-03-08 09:48:45 +0100

    add: BaseScene and testScene  inherits from it

--- universe2/src/scenes/testScene.js
+++ universe2/src/scenes/testScene.js
@@ -2,58 +2,26 @@ import * as THREE from "three";
 import { StarSystem } from "../utils/starSystemHelper.js"
 import { SkyBox } from "../visuals/skyBox.js";
 import { createEntity } from "../factories/entityFactory.js";
-import { loadEntities } from "../utils/loadEntities.js"
+import { BaseScene } from "./baseScene.js"
 
 
-export class TestScene
+export class TestScene extends BaseScene
 {
     // Step 10
     constructor(scene, camera, player, focus = {}) 
     {  
-        console.log("Step 10: testScene.js: constructor");
-        this.active = true;
-        StarSystem.timeFactor=100
+        console.log("TestScene constructor");
+        super(scene, camera, player, focus);
 
+        StarSystem.timeFactor = 100;
+        
         this.SIZE_SCALE = 1;
         this.REGION_SIZE_SCALE = 0.000144 * this.SIZE_SCALE;
         this.LOCAL_SIZE_SCALE = 5 * this.REGION_SIZE_SCALE;
         this.INNER_SIZE_SCALE = 1800 * this.LOCAL_SIZE_SCALE;
-       
         this.near = 16;
         this.far = 16000;
         this.cameraSettings = { near: this.near, far: this.far };
-        this.scene = scene;
-        this.scene.background = SkyBox.Load("StarBox");
-        this.camera = camera;
-        this.player = player;
-        this.focus = focus;
-        this.objects = [];
-        this.objectMap = {};
-    }
-
-    // Step 11
-    async Init() 
-    {
-        console.log("Step 11: testScene.js: async Init() ");
-        if (!this.active) return;
-        const { requiredKeys, scaleMap } = this.GetEntityConfig();
-
-        try {
-            const { entityMap, sizeMap } = await loadEntities(requiredKeys, scaleMap);
-            this.entityMap = entityMap;
-            this.sizeMap = sizeMap;
-            this.CreateObjects();
-            this.PlayerEntryPosition();
-            if (this.SetExitCondition) {
-                this.SetExitCondition();
-            }
-            if (this.DefinePortals) {
-                this.DefinePortals();
-            }
-        } 
-        catch (err) {
-            console.error("Failed to load entities", err);
-        }
     }
 
     GetEntityConfig() 
@@ -192,34 +160,5 @@ export class TestScene
 
         console.log("Setting player facing direction:", 2*distance.x, 2*distance.y, 2*distance.z);
         this.player.FaceTarget(2*distance.x, 2*distance.y, 2*distance.z);
-    }
-
-    Update(dt) 
-    {
-        if (!this.sun) return;
-        for (const obj of this.objects) {
-            obj.Update(dt);
-        }
-        if (this.CheckSceneTransition) {
-            this.CheckSceneTransition();
-        }
-    }
-
-    Dispose() 
-    {
-        this.active = false;
-        this.objects.forEach(obj => obj?.Dispose());
-        this.objects = [];
-        if (this.sceneTriggers) {
-            this.sceneTriggers = [];
-        }
-
-        // Dispose skybox
-        if (this.scene?.background) {
-            SkyBox.Dispose(this.scene.background);
-            this.scene.background = null;
-        }
-        // Clear objectMap to remove references
-        this.objectMap = {};
     }
 }
