5ff00be0f6a02dd9235370ca006a319a7b7f95e5
ales2026 <ale.urankar@gmail.com>
2026-03-21 11:07:44 +0100

    better Landing Page

--- universe2/app/components/LandingPage.tsx
+++ universe2/app/components/LandingPage.tsx
@@ -7,14 +7,22 @@ export default function LandingPage() {
   const router = useRouter();
 
   return (
-    <div className="p-10 max-w-lg my-12 mx-auto border-1 border-gray-600
+    <div className="p-4 mx-6 sm:mx-2 md:mx-4 my-6 border border-gray-600
         rounded-xl bg-gray-900 text-white text-center">
-      <h1 className="text-4xl font-bold mb-8">Welcome to Universe</h1>
 
+      {/* Top: Title */}
+      <h1 className="text-4xl font-bold mt-4 mb-12">This is an Universe Simulation</h1>
+      
+      {/* Middle: Description */}
+      <p className="text-2xl mb-8">
+        This is a universe simulator. It uses real-world data to generate objects and allows you to explore and adjust various scales dynamically.
+      </p>
+      
+      {/* Bottom: Button */}
       <div className="flex flex-col gap-6">
-        <button className="active:bg-blue-900 bg-blue-800 hover:bg-blue-700 py-4 rounded-lg text-2xl font-bold uppercase"
+        <button className="active:bg-blue-900 bg-blue-800 hover:bg-blue-700 py-4 rounded-lg text-2xl font-bold"
           onClick={() => router.push('/universe')}
-        >Enter Universe
+        >Enter the Universe
         </button>
       </div>
     </div>
